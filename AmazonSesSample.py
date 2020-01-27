34c2c70b9922c40cfe88a518b3a840bb4da42ee9
矢野大樹 <daikitech0123@gmail.com>
2020-01-27 20:26:19 +0200

    Add AMAZON SES

---
+++ AmazonSesSample.py
@@ -0,0 +1,99 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import boto3
+from botocore.exceptions import ClientError
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+class EmailService():
+    # Replace sender@example.com with your "From" address.
+    # This address must be verified with Amazon SES.
+    SENDER = "EMAIL"
+
+    # Replace recipient@example.com with a "To" address. If your account 
+    # is still in the sandbox, this address must be verified.
+    RECIPIENT = "EMAIL"
+
+    # Specify a configuration set. If you do not want to use a configuration
+    # set, comment the following variable, and the 
+    # ConfigurationSetName=CONFIGURATION_SET argument below.
+    CONFIGURATION_SET = "ConfigSet"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+    AWS_REGION = "ap-south-1"
+
+    # The subject line for the email.
+    SUBJECT = "【KOBITON】Result of Automation Test"
+
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+                "This email was sent with Amazon SES using the "
+                "AWS SDK for Python (Boto)."
+                )
+                
+    # The HTML body of the email.
+    BODY_HTML = """<html>
+    <head></head>
+    <body>
+    <h1>Amazon SES Test (SDK for Python)</h1>
+    <p>This email was sent with
+        <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+        <a href='https://aws.amazon.com/sdk-for-python/'>
+        AWS SDK for Python (Boto)</a>.</p>
+    </body>
+    </html>
+                """            
+
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+
+    # Create a new SES resource and specify a region.
+    def __init__(self):
+        self.client = boto3.client('ses',
+                            aws_access_key_id=self.AWS_ACCESS_KEY_ID,
+                            aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY,
+                            region_name=self.AWS_REGION)
+
+    def send_result_mail(self,data,kobitonSessionId):
+        # Try to send the email.
+        try:
+            #Provide the contents of the email.
+            response = self.client.send_email(
+                Destination={
+                    'ToAddresses': [
+                        self.RECIPIENT,
+                    ],
+                },
+                Message={
+                    'Body': {
+                        'Html': {
+                            'Charset': self.CHARSET,
+                            'Data': self.BODY_HTML + data['executionData']['desired']['deviceName'] + str(kobitonSessionId),
+                        },
+                        'Text': {
+                            'Charset': self.CHARSET,
+                            'Data': self.BODY_TEXT,
+                        },
+                    },
+                    'Subject': {
+                        'Charset': self.CHARSET,
+                        'Data': self.SUBJECT,
+                    },
+                },
+                Source=self.SENDER,
+                # If you are not using a configuration set, comment or delete the
+                # following line
+                # ConfigurationSetName=CONFIGURATION_SET,
+            )
+        # Display an error if something goes wrong.	
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            print("Email sent! Message ID:"),
+            print(response['MessageId'])
+
+# test = EmailService()  
+# test.send_result_mail()
